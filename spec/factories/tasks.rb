389c61f4d6127bf8aa7f51255655fffdd0e5597d
hide-ki <okashiokashi@icloud.com>
2021-01-10 17:03:58 +0900

    Fix: FactoryBotでsequenceを使ってtitleを作成

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,8 +1,9 @@
 FactoryBot.define do
   factory :task do
-    title { "hoge" }
-    content { "hogehoge"}
-    status { "todo" }
+    sequence(:title, "title_1")
+    content { "content" }
+    status { :todo }
+    deadline { 1.week.from_now }
     association :user
   end
 end
